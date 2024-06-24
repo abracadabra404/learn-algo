e2cb7fc422b0e04b97b8886e9c57bf18bce68f49
xvyaxi <xvyaxi@outlook.com>
2024-06-24 21:22:45 +0800

    merge sort

--- com/aba/algo/sort/MergeSort.java
+++ com/aba/algo/sort/MergeSort.java
@@ -1,5 +1,7 @@
 package com.aba.algo.sort;
 
+import java.util.Arrays;
+
 /**
  * merge sort
  *
@@ -10,6 +12,9 @@ package com.aba.algo.sort;
 public class MergeSort {
 
     public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 6, 4, 5};
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
 
     }
 
@@ -22,19 +27,58 @@ public class MergeSort {
         mergeSortRecursive(arr, 0, length - 1);
     }
 
-    private static void mergeSortRecursive(int[] arr, int start, int end) {
+    private static void mergeSortRecursive(int[] arr, int left, int right) {
 
-        if (start >= end) {
+        if (left >= right) {
             return;
         }
-        int mid = (start + end) / 2;
-        mergeSortRecursive(arr, start, mid);
-        mergeSortRecursive(arr, mid + 1, end);
-//        merge(arr, start, mid, end);
+        int mid = (left + right) / 2;
+        mergeSortRecursive(arr, left, mid);
+        mergeSortRecursive(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 
-//    private static void merge(int[] arr, int start, int mid, int end) {
-//        int[] help = new int[end - start + 1];
-//        int i = 0;
-//    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        // 找到两个子数组的大小
+        int leftLength = mid - left + 1;
+        int rightLength = right - mid;
+        // 申请两个对应大小的数组
+        int[] leftArray = new int[leftLength];
+        int[] rightArray = new int[rightLength];
+        // 把数据复制到这两个数组中
+        for (int i = 0; i < leftLength; i++) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < rightLength; j++) {
+            rightArray[j] = arr[mid + 1 + j];
+        }
+        // 初始化两个数组的指针
+        int i = 0, j = 0;
+        // 初始化合并后数组的指针
+        int index = left;
+        // 合并两个子数组
+        while (i < leftLength && j < rightLength) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[index] = leftArray[i];
+                i++;
+            } else {
+                arr[index] = rightArray[j];
+                j++;
+            }
+            index++;
+        }
+        // 把左边剩余的数组元素复制到合并后数组
+        while (i < leftLength) {
+            arr[index] = leftArray[i];
+            i++;
+            index++;
+        }
+        // 把右边剩余的数组元素复制到合并后数组
+        while (j < rightLength) {
+            arr[index] = rightArray[j];
+            j++;
+            index++;
+        }
+    }
 }
