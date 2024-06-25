f05564460d0ce559ffb3a7fcc91848b3d38d4d64
xvyaxi <xvyaxi@outlook.com>
2024-06-25 23:11:19 +0800

    QuickSort

--- com/aba/algo/sort/QuickSort.java
+++ com/aba/algo/sort/QuickSort.java
@@ -1,14 +1,18 @@
 package com.aba.algo.sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
 
     public static void main(String[] args) {
+        int[] arr = {3,6,8,10,1,2,1};
+        quickSort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
 
     }
 
     public static void quickSort(int[] arr,int left,int right){
-
         if (left < right) {
             // 找到pivot点
             int pi  = partition(arr, left, right);
@@ -16,22 +20,22 @@ public class QuickSort {
             quickSort(arr, left, pi-1);
             quickSort(arr, pi+1, right);
         }
-
-
-
-
     }
 
     private static int partition(int[] arr, int left, int right) {
         int pivot = arr[right];
+        // 较小元素的索引
         int i = left-1;
         for (int j = left; j < right; j++) {
             if (arr[j] <= pivot) {
                 i++;
-//                swap(arr, i, j);
+                // 交换 i，j
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
             }
-            return 0;
         }
-        return 0;
+        return i+1;
     }
 }
