9065ba5e828250d038a891eb9b8b264a20b6338f
xvyaxi <xvyaxi@outlook.com>
2024-06-26 22:00:00 +0800

    QuickSort

--- com/aba/algo/sort/QuickSort.java
+++ com/aba/algo/sort/QuickSort.java
@@ -6,12 +6,23 @@ public class QuickSort {
 
 
     public static void main(String[] args) {
-        int[] arr = {3,6,8,10,1,2,1};
+        int[] arr = {3,6,8,10,1,2};
         quickSort(arr,0,arr.length-1);
         System.out.println(Arrays.toString(arr));
 
     }
 
+    /**
+     * 快速排序
+     * 是一个原地排序算法
+     * 不是一个稳定的排序算法，排序后相同的元素的相对位置会改变
+     * 时间复杂度：平均O(nlogn)，最坏O(n^2)，最好O(nlogn)
+     * 处理过程是由上到下，先分区，再处理子问题
+     * @param: [arr, left, right]
+     * @return: void
+     * @author: abracadabra
+     * @date: 2024/6/26 21:41
+     */
     public static void quickSort(int[] arr,int left,int right){
         if (left < right) {
             // 找到pivot点
@@ -36,6 +47,10 @@ public class QuickSort {
 
             }
         }
+        // 交换pivot
+        int temp = arr[i+1];
+        arr[i+1] = arr[right];
+        arr[right] = temp;
         return i+1;
     }
 }
