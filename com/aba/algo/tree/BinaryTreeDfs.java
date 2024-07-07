8723334cfdf7ff296b5ff369a54d8713baa79d50
xvyaxi <xvyaxi@outlook.com>
2024-07-07 15:21:17 +0800

    dfs

--- com/aba/algo/tree/BinaryTreeDfs.java
+++ com/aba/algo/tree/BinaryTreeDfs.java
@@ -12,11 +12,13 @@ import java.util.List;
  */
 public class BinaryTreeDfs {
 
+
     public final static  List<Integer> res = new ArrayList<>();
 
-
-
-
+    // 前序遍历,访问优先级：
+    // 1.访问根节点
+    // 2.前序遍历左子树
+    // 3.前序遍历右子树
     public static void preOrder(TreeNode root) {
         if (root == null)
             return ;
@@ -25,6 +27,10 @@ public class BinaryTreeDfs {
         preOrder(root.right);
     }
 
+    // 中序遍历,访问优先级：
+    // 1.中序遍历左子树
+    // 2.访问根节点
+    // 3.中序遍历右子树
     public static void inOrder(TreeNode root) {
         if (root == null)
             return ;
@@ -33,6 +39,10 @@ public class BinaryTreeDfs {
         inOrder(root.right);
     }
 
+    // 后序遍历,访问优先级：
+    // 1.后序遍历左子树
+    // 2.后序遍历右子树
+    // 3.访问根节点
     public static void postOrder(TreeNode root) {
         if (root == null)
             return ;
