a7bdfe9ba2261bf8ad3b7f49b1894fde75ba6b13
xvyaxi <xvyaxi@outlook.com>
2024-07-11 21:44:30 +0800

    heightTree

--- com/aba/algo/tree/TreeNode.java
+++ com/aba/algo/tree/TreeNode.java
@@ -17,15 +17,9 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode() {}
     TreeNode(int val) { this.val = val; }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
 
-    }
 
     public static void main(String[] args) {
         TreeNode n1 = new TreeNode(1);
